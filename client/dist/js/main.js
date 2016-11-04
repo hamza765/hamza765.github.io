0ab162b31ded882385459cbff3972bae30e75a65
Hamza <hamz5678@hotmail.com>
2016-11-04 03:47:15 -0500

    No need to make it a node server

--- client/dist/js/main.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$("textarea").autotype("Hello. \nMy name is... Hamza.\nWelcome to my website.\nI am a web developer located in Houston, TX.\nThis website is currently under construction.\nIf you really need me however, my email is Hamz5768@hotmail.com.\n\n\n\nHave a atrsy{{back}}{{back}}{{back}}{{back}}{{back}}great day now!",{delay:40})});
--- server/middleware/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-export default function() {
-    var router = Router();
-
-    
-
-    return router;
-}
